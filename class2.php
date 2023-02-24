fe3814acc24a8b021653ebe131309f8bd13a0630
TiparpronS <Sukanyat2022a@gmail.com>
2023-02-24 10:07:08 -0500

    add

--- class2.php
+++ class2.php
@@ -3,16 +3,22 @@ $rec represent objects of that class -->
 <!-- this page we use pattern to crate object -->
 <!--what ever we want to show on the page we use this page-->
 <!--$_POST or $_GET use to connect html page -->
-<?php
-$length $_POST["lengthforPHP"]
 
-// inside["lengthforPHP"] come from html page
+<!--this page act like middle man connect bet class.php and index.html -->
+
+<?php
 
 include "class.php";
-$rec = new rectangle();//we create object of rectangle
-$rec->setLength(10);
+$length = $_POST["lengthforPHP"];// this class[]inside hole name or id in html page
+$width = $_POST["widthforPHP"];
+// inside["lengthforPHP"] come from html page
 
-$rec->setwidth(20);
+
+$rec = new rectangle();//we create object of rectangle
+$rec->setLength($length);
+
+$rec->setwidth($width);
+
 
 echo "The area of rectangle is ".$rec->findArea();  // . is +
 ?>
